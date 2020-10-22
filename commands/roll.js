a0406050a3c6c4b14ba6d1db3672b15229642dc9
NicolasGerhardt <nicolas.gerhardt@gmail.com>
2020-10-21 23:47:49 -0400

    refactored roll command to utilize static helper

--- commands/roll.js
+++ commands/roll.js
@@ -1,69 +1,37 @@
-import rollHelper from "./roll/rollhelper";
+const rollHelper = require("./roll/rollhelper");
 
 module.exports = {
   name: "roll",
   description: "Roll dice",
   args: true,
   usage:
-    "[number of dice]d[number of sides on dice][+/-][single mondifer] \n--Note: each number must be a 3-digit whole number to work",
+    "Roll dice with standard 'd' notation. You will have to add your mods yourself.  Examples d20, 2d6, 30d4, etc.",
   execute(message, args) {
-    let rollString = rollHelper.buildRollString(args);
+    const rollCommand = rollHelper.mapArgstoRollCommand(args);
 
     try {
-      rollHelper.validate(rollString);
+      rollHelper.validate(rollCommand);
     } catch (error) {
       return message.reply(error.message);
     }
 
-    let rollArray = rollHelper.buildRollArray(rollString);
+    const dice = rollHelper.buildDice(rollCommand);
 
     let rolls = [];
     let totalRoll = 0;
 
-    if (rollArray[0] && rollArray[1] != "pi") {
-      for (let i = 0; i < rollArray[0]; i++) {
-        rolls.push(Math.floor(Math.random() * Math.floor(rollArray[1])) + 1);
-        totalRoll += rolls[i];
-      }
-    } else if (rollArray[1] == "pi") {
-      rolls.push(Math.random() * Math.PI + 1);
-      totalRoll += rolls[0];
-    } else {
-      rolls.push(Math.floor(Math.random() * Math.floor(rollArray[1])) + 1);
-      totalRoll += rolls[0];
-    }
-
-    if (rollArray[2] && rollArray[2] != "pi") {
-      totalRoll += parseInt(rollArray[2]);
-    } else if (rollArray[2] && rollArray[2] == "pi") {
-      totalRoll += Math.PI;
-    }
-
-    if (rollArray[1] == 20 && (rollArray[0] == "" || rollArray[0] == 1)) {
-      // rolling single d20
-      let r = Math.floor(Math.random() * Math.floor(3));
-      if (rolls[0] == 1) {
-        // nat 1
-        if (r == 0) {
-          message.reply("uh oh...");
-        } else if (r == 1) {
-          message.reply("nuts...");
-        } else if (r == 2) {
-          message.reply("better luck next time...");
-        }
-      } else if (rolls[0] == 20) {
-        // nat 20
-        if (r == 0) {
-          message.reply("dope");
-        } else if (r == 1) {
-          message.reply("what are the odds?");
-        } else if (r == 2) {
-          message.reply("go you!");
-        }
-      }
+    for (let i = 0; i < dice.quantity; i++) {
+      rolls.push(rollHelper.rollDieWithsides(dice.sides));
+      totalRoll += rolls[i];
     }
 
     message.channel.send("Each roll: [" + rolls.join("][") + "]");
     message.channel.send("Total roll: " + totalRoll);
+    
+    if (dice.sides == 20) {
+      rolls.forEach((roll) => {
+        rollHelper.critCheers(message, dice.sides, roll);
+      });
+    }
   },
 };
--- commands/roll/rollhelper.js
+++ commands/roll/rollhelper.js
@@ -1,19 +1,42 @@
 module.exports = {
   validate(rollString) {
     // RegEx of valid roll string
-    const regex = new RegExp(/^\d{0,3}d(\d{1,3}|pi)([\+\-](\d{1,3}|pi)){0,1}$/);
+    const regex = new RegExp(/^\d{0,3}d(\d{1,3}|🥧)$/);
 
     if (!regex.test(rollString)) {
       throw new Error(`'${rollString}' is not a properly formated roll`);
     }
   },
-  buildRollString(args) {
-    if (args.length > 1) {
-      return args.join("").toLowerCase();
-    }
-    return (output = args[0].toLowerCase());
+  mapArgstoRollCommand(args) {
+    var output = args[0];
+    output += args.length > 1 ? args.join("") : "";
+    return output.toLowerCase().trim();
   },
-  buildRollArray(rollString) {
-    return rollString.toLowerCase().split(/[d\+\-]/);
+  buildDice(rollCommand) {
+    const dIndex = rollCommand.indexOf("d");
+    return {
+      quantity: dIndex == 0 ? 1 : rollCommand.substring(0, dIndex),
+      sides: rollCommand.substring(dIndex + 1),
+    };
+  },
+  rollDieWithsides(sides) {
+    if (sides == "🥧") return Math.PI;
+
+    return Math.ceil(Math.random() * sides);
+  },
+  critCheers(message, sides, roll) {
+    if (roll == sides) {
+      switch (this.rollDieWithsides(3)) {
+        case 1:
+          message.reply("dope");
+          break;
+        case 2:
+          message.reply("what are the odds?");
+          break;
+        case 3:
+          message.reply("go you!");
+          break;
+      }
+    }
   },
 };

286e65d6fb29436fff881681244cf785db565417
NicolasGerhardt <Nicolas.Gerhardt@gmail.com>
2021-05-19 11:47:58 -0400

    Refactor main (#12)
    * restructured main
    
    * Added Docker Compose and refactored Bot.js a bit
    
    * Fixed Bot commands
    
    * patched args bug
    
    Co-authored-by: Gerhardt, Nicolas (IT) <NicolasGerhardt@quickenloans.com>

--- bot.js
+++ bot.js
@@ -1,45 +1,60 @@
+//////////////////////////////////// Constants
+
 const { prefix } = require("./config.json");
 const { token } = require("./secrets/token");
+
 const fs = require("fs");
+
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
 
-const commandFiles = fs
-  .readdirSync("./commands")
-  .filter((file) => file.endsWith(".js"));
+BindCommands();
 
-for (const file of commandFiles) {
-  const command = require(`./commands/${file}`);
-  client.commands.set(command.name, command);
+//////////////////////////////////// Events
+
+client.once("ready", () => { Log("Ready!~") });
+
+client.on("message", (message) => { processMessage(message) });
+
+//////////////////////////////////// Functions
+
+function BindCommands() {
+  const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+  }
 }
 
-const Filter = require("bad-words");
-const filter = new Filter();
+function Log(message) {
+  console.log(`${new Date()}:${message}`);
+}
 
-client.once("ready", () => {
-  console.log(new Date());
-  console.log("Ready!");
-});
+function GetMessageCommand(message) {
+  var words = message.content.slice(prefix.length).toLowerCase().split(/\s+/);
+  return {
+    name: words.shift(),
+    args: words,
+  };
+}
 
-client.on("message", (message) => {
-  if (filter.isProfane(message.content)) {
-    message.reply("Language!");
-    return;
-  }
-
+function processMessage(message) {
   if (!message.content.startsWith(prefix) || message.author.bot) return;
+  
+  Log(`${message.author.username}:${message.content}`);
 
-  console.log(message.author.username + ": " + message.content); //log what was said to bot
-  const args = message.content.slice(prefix.length).split(/\s+/);
-  const commandName = args.shift().toLowerCase();
+  const messageCommand = GetMessageCommand(message);
 
-  if (!client.commands.has(commandName)) return;
+  if (!client.commands.has(messageCommand.name)) return;
 
-  const command = client.commands.get(commandName);
+  const command = client.commands.get(messageCommand.name);
 
-  if (command.args && !args.length) {
+  if (command.args && !messageCommand.args.length) {
     let reply = `You didn't provide any arguments, ${message.author}!`;
 
     if (true) {
@@ -50,11 +65,13 @@ client.on("message", (message) => {
   }
 
   try {
-    command.execute(message, args);
+    command.execute(message, messageCommand.args);
   } catch (error) {
     console.error(error);
     message.reply("there was an error trying to execute that command!");
   }
-});
+}
+
+//////////////////////////////////// Connect to Discord
 
 client.login(token);
